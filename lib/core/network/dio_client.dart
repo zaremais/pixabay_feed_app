023d6589d5290133b0735d3cb892110e1085e145
zaremais <115091187+zaremais@users.noreply.github.com>
2025-11-21 15:16:38 +0600

    add_logger

--- lib/core/network/dio_client.dart
+++ lib/core/network/dio_client.dart
@@ -1,29 +1,38 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 import 'package:pixabay_image_feed/core/conatants/api_url.dart';
+import 'package:pixabay_image_feed/core/network/dio_client.dart' as dio;
 import 'package:pixabay_image_feed/core/network/intercetptors/token_interseptor.dart';
 
 @injectable
 class DioClient {
-  late final Dio dio;
+  final Dio dio;
 
-  DioClient() {
-    dio = Dio(
-      BaseOptions(
-        baseUrl: ApiUrls.baseUrl,
-        connectTimeout: const Duration(seconds: 10),
-        receiveTimeout: const Duration(seconds: 10),
-        responseType: ResponseType.json,
-      ),
-    );
-
+  DioClient()
+    : dio = Dio(
+        BaseOptions(
+          baseUrl: ApiUrls.baseUrl,
+          connectTimeout: const Duration(seconds: 10),
+          receiveTimeout: const Duration(seconds: 10),
+        ),
+      ) {
     dio.interceptors.add(TokenInterceptor());
+
+    dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
   }
 
   Future<Response> get(
     String path, {
-    Map<String, dynamic>? queryParameters,
-  }) async {
-    return await dio.get(path, queryParameters: queryParameters);
+    Map<String, dynamic>? query,
+    required Map<String, Object> queryParameters,
+  }) {
+    return dio.get(path, queryParameters: query);
   }
+}
+
+Future<Response> get(
+  String path, {
+  Map<String, dynamic>? queryParameters,
+}) async {
+  return await dio.get(path, queryParameters: queryParameters);
 }
